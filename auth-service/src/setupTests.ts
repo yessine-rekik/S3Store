bae4b1d7f4493c41ac0aed627f92338c1e629e4d
yessine99 <yessine.rekik@esprit.tn>
2024-06-02 17:44:07 +0200

    fixed env vars for ci tests

--- auth-service/src/setupTests.ts
+++ auth-service/src/setupTests.ts
@@ -4,12 +4,14 @@ import mongoose from 'mongoose';
 let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+
   process.env.ACCESS_TOKEN_SECRET = 'azerty';
   process.env.REFRESH_TOKEN_SECRET = 'qwerty';
   process.env.PORT = '4001';
+  process.env.MONGODB_URI = mongoServer.getUri();
 
-  mongoServer = await MongoMemoryServer.create();
-  await mongoose.connect(mongoServer.getUri());
+  await mongoose.connect(process.env.MONGODB_URI);
 });
 
 afterAll(async () => {
