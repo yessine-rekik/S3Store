a1889d6724e60a0adac886b943cc10ec2c851ce4
yessine99 <yessine.rekik@esprit.tn>
2024-06-09 20:29:36 +0200

    fixed cors

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -3,12 +3,20 @@ import { errorHandler } from './middlewares/errorHandler';
 import { logApiInfo } from './middlewares/logApiInfo';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import config from './config';
+import cors from 'cors';
 
 const app = express();
 
 app.use(logApiInfo);
 
 app.use(errorHandler);
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
 app.use(
   '/api/auth',
