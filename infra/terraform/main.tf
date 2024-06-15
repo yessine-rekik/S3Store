2f72d9078c0aaee857b89f844b4667730868e090
yessine99 <rrekikyessine@gmail.com>
2024-06-16 00:50:45 +0100

    auto sync images

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -58,5 +58,12 @@ resource "helm_release" "loki-stack" {
   }
 }
 
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+}
 
 
