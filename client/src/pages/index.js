87fcceefe2d8ae6f9ee44ca47449539263aeb166
yessine99 <rrekikyessine@gmail.com>
2024-06-16 23:57:48 +0100

    fixing img updates

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,7 +6,7 @@ const Index = () => {
       <Head>
         <title>S3Store</title>
       </Head>
-      <h1>Welcome To the update: vmdlkfmdlfgk</h1>
+      <h1>Welcome To the update: hello world</h1>
     </>
   );
 };
