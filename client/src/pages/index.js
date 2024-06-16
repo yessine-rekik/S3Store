ba50e8c560f1a9c2d3e13970f9e3bcf213e4a9c5
yessine99 <rrekikyessine@gmail.com>
2024-06-17 00:54:19 +0100

    image auto update fixed

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,7 +6,7 @@ const Index = () => {
       <Head>
         <title>S3Store</title>
       </Head>
-      <h1>Welcome To the update: it's fixed !!!!!!!!!!!!! ?</h1>
+      <h1>Welcome to the Homelab!</h1>
     </>
   );
 };
