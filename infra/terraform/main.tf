0c7c6f721c563377f616db7c82bdc947d57a7514
yessine99 <rrekikyessine@gmail.com>
2024-06-17 12:41:07 +0100

    readded prometheus to terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -49,27 +49,29 @@ resource "helm_release" "ingress-nginx" {
   wait             = false
 }
 
-# resource "helm_release" "kube-prometheus-stack" {
-#   name       = "prometheus"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart      = "kube-prometheus-stack"
-#   namespace  = "monitoring"
-# }
+resource "helm_release" "kube-prometheus-stack" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  version    = "60.2.0"
+}
 
-# resource "helm_release" "loki-stack" {
-#   name       = "loki"
-#   repository = "https://grafana.github.io/helm-charts"
-#   chart      = "loki-stack"
-#   namespace  = "monitoring"
-#   set {
-#     name  = "loki.image.tag"
-#     value = "2.9.3"
-#   }
-#   set {
-#     name  = "promtail.enabled"
-#     value = "true"
-#   }
-# }
+resource "helm_release" "loki-stack" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-stack"
+  namespace  = "monitoring"
+  version    = "2.10.2"
+  set {
+    name  = "loki.image.tag"
+    value = "2.9.3"
+  }
+  set {
+    name  = "promtail.enabled"
+    value = "true"
+  }
+}
 
 resource "helm_release" "argocd" {
   name       = "argocd"
