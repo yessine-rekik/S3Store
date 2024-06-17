24d0dc0024fded9fd294bf0c9cd4235c6e437e84
yessine99 <rrekikyessine@gmail.com>
2024-06-17 19:23:13 +0100

    updating client api base url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,11 +1,6 @@
 const configVariables = {
-  ENV: process.env.NEXT_PUBLIC_ENV || 'dev',
-
-  DEV_BASE_URL: 'http://localhost:4000/api',
-
-  STAGING_BASE_URL: 'http://staging.s3store.com/api',
-
-  PROD_BASE_URL: 'http://s3store.com/api',
+  ENV: process.env.NEXT_PUBLIC_ENV || 'local',
+  BASE_URL: process.env.NEXT_PUBLIC_ENV === 'local' ?  'http://localhost:4000/api' : '/api'
 };
 
 export default configVariables;
