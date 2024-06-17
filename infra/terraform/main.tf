2142458e7e289f5cbe611fde2818fad9499d6175
yessine99 <rrekikyessine@gmail.com>
2024-06-17 12:13:40 +0100

    cleaning and commenting prometheus from terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -44,37 +44,39 @@ resource "helm_release" "ingress-nginx" {
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   namespace        = "ingress-nginx"
+  version          = "4.10.1"
   create_namespace = true
   wait             = false
 }
 
-resource "helm_release" "kube-prometheus-stack" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace  = "monitoring"
-}
+# resource "helm_release" "kube-prometheus-stack" {
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   namespace  = "monitoring"
+# }
 
-resource "helm_release" "loki-stack" {
-  name       = "loki"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "loki-stack"
-  namespace  = "monitoring"
-  set {
-    name  = "loki.image.tag"
-    value = "2.9.3"
-  }
-  set {
-    name  = "promtail.enabled"
-    value = "true"
-  }
-}
+# resource "helm_release" "loki-stack" {
+#   name       = "loki"
+#   repository = "https://grafana.github.io/helm-charts"
+#   chart      = "loki-stack"
+#   namespace  = "monitoring"
+#   set {
+#     name  = "loki.image.tag"
+#     value = "2.9.3"
+#   }
+#   set {
+#     name  = "promtail.enabled"
+#     value = "true"
+#   }
+# }
 
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = "argocd"
+  version    = "7.1.3"
 }
 
 resource "helm_release" "argocd-image-updater" {
@@ -82,6 +84,7 @@ resource "helm_release" "argocd-image-updater" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argocd-image-updater"
   namespace  = "argocd"
+  version    = "0.10.2"
 }
 
 
