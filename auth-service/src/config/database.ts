38e09e156388e618b581578b6ed33157891f880a
yessine99 <yessine.rekik@esprit.tn>
2024-06-23 17:16:52 +0200

    added healthz and readyz endpoints

--- auth-service/src/config/database.ts
+++ auth-service/src/config/database.ts
@@ -3,13 +3,31 @@ import config from '.';
 import logger from '../utils/logger';
 
 const MONGODB_URI = config.MONGODB_URI;
+let isDatabaseConnected = false;
 
 mongoose
-  .connect(MONGODB_URI, {})
-  .then(() => logger.info('DB Connected'))
+  .connect(MONGODB_URI, { dbName: 'auth' })
+  .then(() => {
+    logger.info('DB Connected');
+    isDatabaseConnected = true;
+  })
   .catch((err) => {
     logger.error(
       `Error connecting to the Database: ${err.message}\nProcess will exist.`
     );
     process.exit(1);
   });
+
+mongoose.connection.on('disconnected', () => {
+  logger.error('DB Disconnected');
+  isDatabaseConnected = false;
+});
+
+mongoose.connection.on('reconnected', () => {
+  logger.info('DB Reconnected');
+  isDatabaseConnected = true;
+});
+
+export const getDatabaseSatus = () => {
+  return isDatabaseConnected;
+};
